16c0b07843f57889799df06b239eed89dbe79bea
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-03-03 10:47:48 +0200

    Update CHECKLIST Component
    Update CHECKLIST component

--- app/CheckList.js
+++ app/CheckList.js
@@ -1,21 +1,55 @@
 import React, { Component } from 'react';
 
 class CheckList extends Component {
+    
+    checkInputKeyPress(e){
+        if(e.key === 'Enter'){
+            this.props.taskCallbacks.add(this.props.cardId,e.target.value);
+            e.target.value='';
+        }
+    }
+    
+    
+    
   render() {
-    let tasks = this.props.tasks.map((task) => (
-      <li className="checklist__task">
-        <input type="checkbox" defaultChecked={task.done} />
-        {task.name}
-        <a href="#" className="checklist__task--remove" />
-      </li>
+    let tasks = this.props.tasks.map((task,taskIndex) => (
+     <li>
+        //checkbox
+        <input 
+            type="checkbox"
+            checked={task.done}
+            onChange={this.props.taskCallbacks.toggle.bind(null, this.props.cardId,task.id, taskIndex) }
+        />
+        //task name
+            {task.name}{' '}
+                                     
+        //remove anchor button
+        <a href="#"
+            className="checklist__task--remove"
+            onClick={this.props.taskCallbacks.delete.bind(null, this.props.cardId, task.id,taskIndex )} />
+                                     
+   
+     </li>
     ));
 
     return (
       <div className="checklist">
         <ul>{tasks}</ul>
+        <input 
+            type="text"
+            placeholder="Add a task and hit Enter"
+            className="checklist--add-task"
+            onKeyPress={this.checkInputKeyPress.bind(this)}
+        />
       </div>
     );
   }
 }
+
+CheckList.propTypes = {
+  cardId: PropTypes.number,
+  tasks: PropTypes.arrayOf(PropTypes.object),
+  taskCallbacks: PropTypes.object
+};
 
 export default CheckList;
