5c8029a657b38c5b88109ce9d77349f823086de4
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-03-03 10:37:16 +0200

    fix Card component

--- app/Card.js
+++ app/Card.js
@@ -1,5 +1,22 @@
 import React, { Component } from 'react';
 import CheckList from './CheckList';
+import marked from 'marked';
+//validate titlePropName
+let titlePropType = (props, propName, componentName) => {
+  if (props[propName]) {
+    let value = props[propName];
+    if (typeof value !== 'string' || value.length > 80) {
+      return new Error(
+        `${propName} in ${componentName} is longer than 80 characters`
+      );
+    }
+  }
+};
+
+
+
+
+
 
 class Card extends Component {
   constructor() {
@@ -8,26 +25,56 @@ class Card extends Component {
       showDetails: false
     }
   };
+    
+    toggleDetails() {
+        this.setState({
+            showDetails:!this.state.showDetails
+        });
+    }
   
   render() {
     let cardDetails;
+      
     if (this.state.showDetails) {
       cardDetails = (
         <div className="card__details">
-          {this.props.description}
-          <CheckList cardId={this.props.id} tasks={this.props.tasks} />
+          <span dangerouslySetInnerHTML={{__html:marked(this.props.description)}} />
+       
+          <CheckList cardId={this.props.id} tasks={this.props.tasks} 
+          taskCallbacks={this.props.taskCallbacks} />
         </div>
       );
     };
+      
+      
+      
+      
+      
+      
+    //add styles
+    let sideColor={};
     return (
       <div className="card">
-        <div className="card__title" onClick={
-          ()=>this.setState({showDetails: !this.state.showDetails})
-        }>{this.props.title}</div>
+        <div style={sideColor}>
+        <div className={
+            this.state.showDetails? "card__title card__title--is-open" : "card__title"
+          } onClick={this.toggleDetails.bind(this)}>
+          {this.props.title}
+        </div>
         {cardDetails}
       </div>
     );
   }
 }
+
+
+Card.propTypes = {
+  id: PropTypes.number,
+  title: titlePropType,
+  description: PropTypes.string,
+  color: PropTypes.string,
+  tasks: PropTypes.arrayOf(PropTypes.object),
+  taskCallbacks: PropTypes.object,
+};
 
 export default Card;
