7f693ca018e5f5c87282c98f90507e077d3476a3
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-03-03 09:55:54 +0200

    add task :fire:

--- app/KanBanBoardContainer.js
+++ app/KanBanBoardContainer.js
@@ -39,6 +39,62 @@ class KanBanBoardContainer extends Component {
         });
     
     }
+    //addTask
+    addTask(cardId,taskName){
+        //keep the ref to the original state 
+        let prevState = this.state;
+        //find the index of card 
+        let cardIndex = this.state.cards.findIndex((card) => card.id == cardId);
+        //create a new task with the given name and a id 
+        let newTask = {
+            id:Data.now(),
+            name:taskName,
+            done:false
+        };
+        //create a new object and push the new task to the array of tasks
+        let nextState = update(this.state.cards, {
+            [cardIndex]:{
+                tasks: {$push:[newTask] }
+            }
+        });
+        
+        //set the component state to the mutated obj
+        this.setState({
+            cards:nextState
+        });
+        //call the api to add the task on the server
+        fetch(`${API_URL}/cards/${cardId}/tasks`,{
+            method:'post',
+            headers:API_HEADERS,
+            body:json.stringify(newTask)
+        })
+            .then((res) => {
+            if(res.ok) {
+                return res.json()
+            }
+            else {
+                throw new Error('Server response has some problem');
+            }
+        })
+            .then((resData) => {
+            //when the server returns the definitive id used for the new task on the server update it on React
+            newTask.id = resData.id
+            this.setState({
+                cards:nextState
+            });
+        })
+            .catch((err) => {
+            this.setState(prevState);
+        })
+        
+        
+        
+        
+        
+        
+        
+        
+    }
     
     
     
