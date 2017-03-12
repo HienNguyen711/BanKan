d55036591f6976742d46e99ddad7954a059533ce
HienNguyen711 <hienminhnguyen711@gmail.com>
2017-03-12 19:44:36 +0200

    dragndrop

--- code_practice/ReactCssTransitionGroupElement/dragndrop/ShoppingCart.js
+++ code_practice/ReactCssTransitionGroupElement/dragndrop/ShoppingCart.js
@@ -1,12 +1,46 @@
 //import.....
 
+//shoppingcart specs: for dragndrop
+const shoppingcartSpects = {
+  drop(){
+    return {
+      name:'ShoppingCart'
+    };
+  };
+
+//hover: called when an item is hovered over the component
+//canDrop:use to specify whether the drop target is able to accept the item
+
+}
+// shopping cart drop target - collect
+/*
+connect: instance of droptargetconnector- use it to assign the drop target role to a DOM node
+monitor: an instance of droptargetmonitor
+canDrop()----- isOver()----- didDrop()
+
+*/
+
+let collect = (connect,monitor) => {
+  return {
+    connectDropTarget:connect.dropTarget(),
+    isOver:monitor.isOver(),
+    canDrop:monitor.canDrop()
+
+
+  }
+}
+
+
+
 
 class ShoppingCard extends Component {
   render() {
+    const {canDrop, isOver, connectDropTarget} = this.props;
+    const isActive = canDrop && isOver;
     return (
       <div>
 
-        Drag here to order!!
+        {isActive?'Thanks for order':'Drag here to order'}
 
 
       </div>
@@ -19,3 +53,5 @@ class ShoppingCard extends Component {
 
   }
 }
+//shopping card propTypes
+
